2c4b52eaf13bd3c8675a56d77dfdcdd8b20b09c3
JeromeRReduta <38510370+JeromeRReduta@users.noreply.github.com>
2020-01-21 22:06:25 -0800

    Made subclasses for each type of robotAI: myHQ, drone, FCenter. Made miners figure out whether they should build or mine based on where they were spawned relative to myHQ.

--- src/jeromeTestPlayer/RobotPlayer.java
+++ src/jeromeTestPlayer/RobotPlayer.java
@@ -10,15 +10,11 @@ import battlecode.common.*;
 public strictfp class RobotPlayer {
     static RobotController rc;
 
-    //TODO: Is modifying static vars against the rules?
-
-    // Tracks locations of both own hq and enemy hq
     static MapLocation myHQ;
-    static int[] enemyHQ = new int[]{-1, -1};
+    static MapLocation pos;
     static Direction mySide;
-    static int directionInt;
 
-    static int dronesBuilt = 0;
+    static int directionInt;
 
     static Direction[] directions = {
             Direction.NORTH,
@@ -33,7 +29,7 @@ public strictfp class RobotPlayer {
     static RobotType[] spawnedByMiner = {RobotType.REFINERY, RobotType.VAPORATOR, RobotType.DESIGN_SCHOOL,
             RobotType.FULFILLMENT_CENTER, RobotType.NET_GUN};
 
-    static int turnCount;
+    static int turnCount = 0;
 
     /**
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
@@ -60,15 +56,34 @@ public strictfp class RobotPlayer {
                 // You can add the missing ones or rewrite this into your own control structure.
                  switch (rc.getType()) {
                     case HQ:
-                        runHQ();
+                        MyHQAI hq = new myHQAI();
+                        hq.run();
+                        break;
+
                        
-                    case MINER:              runMiner();             break;
+                    case MINER:
+                        MinerAI miner = new MinerAI();
+                        miner.run();
+                        break;
+
+
                     case REFINERY:           runRefinery();          break;
                     case VAPORATOR:          runVaporator();         break;
                     case DESIGN_SCHOOL:      runDesignSchool();      break;
-                    case FULFILLMENT_CENTER: runFulfillmentCenter(); break;
+                    
+                    case FULFILLMENT_CENTER:
+
+                        FCenterAI fCenter = new FCenterAI();
+                        fCenter.run();
+                        break;
+
+
                     case LANDSCAPER:         runLandscaper();        break;
-                    case DELIVERY_DRONE:    runDeliveryDrone(); break;
+                    case DELIVERY_DRONE:
+                        DroneAI drone = new DroneAI();
+                        drone.run();
+                        break;
+
                     case NET_GUN:            runNetGun();            break;
                 }
 
@@ -84,211 +99,6 @@ public strictfp class RobotPlayer {
             }
 
         System.out.println("ERROR: It was a good run. *BOOM* ");
-    }
-
-    static void initHQ() throws GameActionException {
-        myHQ = rc.getLocation();
-        if(myHQ.x < rc.getMapWidth()/2) {
-            mySide = Direction.WEST;
-            directionInt = 3;
-        } else {
-            mySide = Direction.EAST;
-            directionInt = 1;
-        }
-
-        rc.submitTransaction(new int[] {myHQ.x, myHQ.y, 11211999, 5963, directionInt}, 8);
-        System.out.println(Clock.getBytecodeNum());
-
-    }
-
-
-
-    static void runHQ() throws GameActionException {
-        initHQ();
-        System.out.println(Clock.getBytecodeNum());
-
-        while (true) {
-            turnCount++;
-            updateHQ();
-        }
-        
-    }
-
-    static void updateHQ() throws GameActionException {
-        for (Direction dir : directions)
-            tryBuild(RobotType.MINER, dir);
-    }
-
-    static void runMiner() throws GameActionException {
-        tryBlockchain();
-        tryMove(randomDirection());
-        if (tryMove(randomDirection()))
-            System.out.println("I moved!");
-        // tryBuild(randomSpawnedByMiner(), randomDirection());
-        for (Direction dir : directions)
-            tryBuild(RobotType.FULFILLMENT_CENTER, dir);
-        for (Direction dir : directions)
-            if (tryRefine(dir))
-                System.out.println("I refined soup! " + rc.getTeamSoup());
-        for (Direction dir : directions)
-            if (tryMine(dir))
-                System.out.println("I mined soup! " + rc.getSoupCarrying());
-    }
-
-    static void runRefinery() throws GameActionException {
-        // System.out.println("Pollution: " + rc.sensePollution(rc.getLocation()));
-    }
-
-    static void runVaporator() throws GameActionException {
-
-    }
-
-    static void runDesignSchool() throws GameActionException {
-
-    }
-
-    /*
-
-    Assuming fulfillment center (1) has enough soup to build a drone and (2) has built < 12 drones,
-    Looks to build a drone either northwest or southeast of itself whenever possible.
-
-    Updates dronesBuilt count accordingly
-
-    */
-
-
-    static void runFulfillmentCenter() throws GameActionException {
-
-        // Which corner F is on (usu. SW or NE)
-        Direction cornerLoc = initFulfillmentCenter();
-
-        while (true) {
-            turnCount++;
-
-            updateFulfillmentCenter(cornerLoc);
-            Clock.getBytecodeNum();
-            Clock.yield();
-        }
-
-        
-
-    }
-
-    static Direction initFulfillmentCenter() throws GameActionException{
-        readInitialMessage();
-
-        MapLocation pos = rc.getLocation();
-
-        // if myHQ is right of F, then F is probably on SW corner - build SW
-        if (myHQ.x - pos.x > 0)
-            return Direction.SOUTHWEST;
-
-        // if myHQ is left of F, then F is probably on NE corner - build NE
-        return Direction.NORTHEAST;
-    }
-
-     static void updateFulfillmentCenter(Direction cornerLoc) throws GameActionException {
-        int droneLimit = 12;
-        boolean maxDrones = dronesBuilt < droneLimit;
-        boolean enoughSoup = rc.getTeamSoup() >= RobotType.DELIVERY_DRONE.cost;
-
-        if (enoughSoup && !maxDrones)
-            if (tryBuild(RobotType.DELIVERY_DRONE, cornerLoc))
-                dronesBuilt++;
-        }
-
-    static void runLandscaper() throws GameActionException {
-
-    }
-
-    static void runDeliveryDrone() throws GameActionException {
-        Direction dir = initDeliveryDrone();
-        MapLocation pos;
-
-        // whether or not the robot couldn't move last turn, true by default
-        boolean wasStuck = true;
-        boolean onPath = true;
-
-        while (true) {
-            turnCount++;
-
-            pos = rc.getLocation();
-
-            if (onPath)
-                wasStuck = patrol(pos, dir, wasStuck);
-
-            else if (!onPath)
-                returnToPath(pos);
-
-            Clock.getBytecodeNum();
-            Clock.yield();
-        }
-
-
-/*
-
-
-        Team enemy = rc.getTeam().opponent();
-        if (!rc.isCurrentlyHoldingUnit()) {
-            // See if there are any enemy robots within striking range (distance 1 from lumberjack's radius)
-            RobotInfo[] robots = rc.senseNearbyRobots(GameConstants.DELIVERY_DRONE_PICKUP_RADIUS_SQUARED, enemy);
-
-            if (robots.length > 0) {
-                // Pick up a first robot within range
-                rc.pickUpUnit(robots[0].getID());
-                System.out.println("I picked up " + robots[0].getID() + "!");
-            }
-        } else {
-            // No close robots, so search for robots within sight radius
-            tryMove(randomDirection());
-        } */
-    }
-
-    static Direction initDeliveryDrone() throws GameActionException{
-        readInitialMessage();
-
-        MapLocation pos = rc.getLocation();
-
-        // If myHQ is to right of drone, prob on NW corner - should go up
-        if (myHQ.x > pos.x)
-            return Direction.NORTH;
-
-        // If myHQ is to left of drone, prob on SE corner - should go down
-        return Direction.SOUTH;
-    }
-
-    // updates delivery drone, and tells drone whether it has moved this turn
-    // true = has moved this turn, false = has not moved (stuck in spot)
-    static boolean patrol(MapLocation pos, Direction dir, boolean wasStuck) throws GameActionException {
-        boolean isAtCorner = pos.distanceSquaredTo(myHQ) == 18;
-        Direction newDir = dir;
-
-        // Don't try to turn drone if it was stuck on current location
-        // If drone begins its turn at corner of its path, and it's its first turn on
-        // the corner, turn it clockwise by 90 deg (rotateRight() twice)
-        if (!wasStuck && isAtCorner)
-            newDir = dir.rotateRight().rotateRight();
-
-        if (tryMove(newDir))
-            return false;
-
-        return true;
-
-
-    }
-
-    // Attempts to move towards myHQ, first straight, then slightly angled right, then slightly angled left
-    // If it can't move in any of these cases, cries
-
-    static void returnToPath(MapLocation pos) throws GameActionException {
-        Direction newDir = pos.directionTo(myHQ);
-
-        if (!tryMove(newDir) && tryMove(newDir.rotateRight()) && tryMove(newDir.rotateLeft()))
-             System.out.println("returnToPath(): I'm stuck!");
-    }
-
-    static void runNetGun() throws GameActionException {
-
     }
 
     /**
@@ -411,7 +221,24 @@ public strictfp class RobotPlayer {
         }
     }
 */
-        static void readInitialMessage() throws GameActionException {
+    public void sendInitialMessage() throws GameActionException {
+        pos = rc.getLocation();
+
+        if (pos.x < rc.getMapWidth()/2) {
+            mySide = Direction.WEST;
+            directionInt = 3;
+        }
+
+        else {
+            mySide = Direction.EAST;
+            directionInt = 1;
+        }
+
+        rc.submitTransaction(new int[] {pos.x, pos.y, 11211999, 5963, directionInt}, 8);
+        System.out.println(Clock.getBytecodeNum());
+    }
+
+    public void readInitialMessage() throws GameActionException {
         int round = 1;
         Transaction[] roundBlock;
 
@@ -435,4 +262,38 @@ public strictfp class RobotPlayer {
             round++;
         }
     }
+
+
+    // Getters and updaters - allow subclasses to interact w/ parent class's variables
+    public MapLocation getPos() throws GameActionException {
+        return pos;
+    }
+
+    // Updates pos to current position
+
+    public void updatePos() throws GameActionException {
+        pos = rc.getLocation();
+    }
+
+    // Returns location of myHQ
+    public MapLocation findMyHQ() throws GameActionException {
+        return myHQ;
+    }
+
+    public int getTurnCount() throws GameActionException {
+        return turnCount;
+    }    
+
+    public void updateTurnCount() throws GameActionException {
+        turnCount++;
+    }
+
+    public Direction getMySide() throws GameActionException {
+        return mySide;
+    }
+
+    public int getDirectionInt() throws GameActionException {
+        return directionInt;
+    }
+
 }
